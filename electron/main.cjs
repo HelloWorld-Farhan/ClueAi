796c3086bacb0221a55eb25c85728e7658b773a5
Farhan Khalid <farhankhalid17968@gmail.com>
2026-07-10 22:42:14 +0530

    Fix ReferenceError causing global shortcuts to fail registration

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, desktopCapturer, session, screen } = require('electron');
+const { app, BrowserWindow, ipcMain, desktopCapturer, session, screen, globalShortcut } = require('electron');
 const path = require('path');
 const audio = require('win-audio');
 
